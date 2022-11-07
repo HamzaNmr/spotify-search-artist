1c572a68b9c5edc30441b801a7a0a863aa0e7657
Hamza <hamza.nmr.acc@gmail.com>
2022-11-07 10:44:08 +0200

    change

--- client/src/components/AlbumCard/AlbumCard.jsx
+++ client/src/components/AlbumCard/AlbumCard.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import Rating from '@mui/material/Rating';
 import { CardActionArea } from '@mui/material';
 
 import useStyles from './styles';
